5eb358fe64581c5c8dcbe08e743e9a08d0d17272
sennmii <lantuongcaongoc@gmail.com>
2026-02-08 17:40:37 +0700

    Rigister Page

--- frontend/user/src/pages/Login/Login.jsx
+++ frontend/user/src/pages/Login/Login.jsx
@@ -1,31 +1,67 @@
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import "./Login.css";
 import logologin from "../../assets/logologin.jpg";
 
 export default function Login() {
+
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className="login-wrapper">
       <div className="login-container">
+
         {/* LEFT */}
         <div className="login-left">
           <h1>
             Welcome to <br /> PandoraPro!!
           </h1>
-         <img src={logologin} alt="logo" className="logo-left" />
+          <img src={logologin} alt="logo" className="logo-left" />
         </div>
 
         {/* RIGHT */}
         <div className="login-right">
           <h2>Đăng nhập</h2>
-          <input type="text" placeholder="Nhập địa chỉ Email" />
-          <input type="password" placeholder="Password" />
-          <p className="register-text">Chưa có tài khoản? <a href="/register">Đăng ký ngay</a></p>
 
-          <button>Đăng nhập</button>
-          <p className="login-divider"> <span>Hoặc đăng nhập với</span></p>
+          <input
+            className="input-field"
+            type="text"
+            placeholder="Nhập địa chỉ Email"
+          />
+
+          <div className="password-field">
+            <input
+              className="input-field"
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+            />
+
+            <span
+              className="eye-icon"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </span>
+          </div>
+
+          <div className="extra-row">
+            <p className="login-text">Quên mật khẩu?</p>
+            <p className="register-text">
+              Chưa có tài khoản? <a href="/register">Đăng ký ngay</a>
+            </p>
+          </div>
+
+          <button className="login-btn">Đăng nhập</button>
+
+          <p className="login-divider">
+            <span>Hoặc đăng nhập với</span>
+          </p>
+
           <div className="google-btn">
             Sign up with Google
           </div>
         </div>
+
       </div>
     </div>
   );
--- frontend/user/src/routes/AppRouter.jsx
+++ frontend/user/src/routes/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/home/homePage.jsx";
 import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 function AppRouter() {
   return (
@@ -8,6 +9,7 @@ function AppRouter() {
       <Routes>
         <Route path="/Login" element={<Login />} />
         <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
